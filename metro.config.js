8ee2c538b8fa4c4182fb294dccfde790796d665a
TheAlphaRice <shampow1@gmail.com>
2022-02-02 16:42:01 -0600

    fixed reactotron

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,7 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+  resolver: {
+    blacklistRE: blacklist(['<rootDir>/ios/Pods/']),
   },
 };
